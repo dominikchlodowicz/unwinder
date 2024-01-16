13e6de12db660eeb5a5ba8c1dc2a93718a7e8ffb
dominikchlodowicz <dominikchlodowicz@gmail.com>
2024-01-16 20:17:26 +0100

    Start working on frontend tests, complete main-flight-search-form component tests.

---
+++ unwinder/ClientApp/src/app/components/flight-search-form/main-flight-search-form/main-flight-search-form.component.spec.ts
@@ -0,0 +1,191 @@
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
+import { MainFlightSearchFormComponent } from './main-flight-search-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { WhereToFlightSearchFormComponent } from '../where-to-flight-search-form/where-to-flight-search-form.component';
+import { WhichWeekendFlightSearchComponent } from '../which-weekend-flight-search-form/which-weekend-flight-search-form.component';
+import { OriginFlightSearchFormComponent } from '../origin-flight-search-form/origin-flight-search-form.component';
+import { PassengersFlightSearchFormComponent } from '../passengers-flight-search-form/passengers-flight-search-form.component';
+
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlightSearchSubmitService } from '../../../services/flight-search-form/flight-search-submit.service';
+import { of, throwError } from 'rxjs';
+
+describe('MainFlightSearchFormComponent', () => {
+  let component: MainFlightSearchFormComponent;
+  let fixture: ComponentFixture<MainFlightSearchFormComponent>;
+  let mockSnackBar: MatSnackBar;
+  let mockFlightSearchSubmitService: jest.Mocked<FlightSearchSubmitService>;
+
+  beforeEach(async () => {
+    // Creating mocks with Jest
+    mockSnackBar = { open: jest.fn() } as unknown as MatSnackBar;
+    mockFlightSearchSubmitService = {
+      submitFlightSearchDataToApi: jest.fn(),
+    } as unknown as jest.Mocked<FlightSearchSubmitService>;
+
+    await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        WhereToFlightSearchFormComponent,
+        WhichWeekendFlightSearchComponent,
+        OriginFlightSearchFormComponent,
+        PassengersFlightSearchFormComponent,
+        MatStepperModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        FormBuilder,
+        { provide: MatSnackBar, useValue: mockSnackBar },
+        {
+          provide: FlightSearchSubmitService,
+          useValue: mockFlightSearchSubmitService,
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainFlightSearchFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize form groups correctly', () => {
+    component.ngOnInit();
+    expect(component.whereFormGroup).toBeDefined();
+    expect(component.originFormGroup).toBeDefined();
+    expect(component.whenFormGroup).toBeDefined();
+    expect(component.passengersFormGroup).toBeDefined();
+  });
+
+  it('should reference subcomponents correctly after view init', () => {
+    component.ngAfterViewInit();
+    expect(component.citiesAutocompleteWhereTo).toBeDefined();
+    expect(component.originFlightSearchFormComponent).toBeDefined();
+    expect(component.wichWeekendFlightSearchComponent).toBeDefined();
+    expect(component.passengersFlightSearchFormComponent).toBeDefined();
+  });
+
+  it('should add form controls to form groups after view init', fakeAsync(() => {
+    component.ngOnInit();
+    component.ngAfterViewInit();
+    tick();
+    expect(component.whereFormGroup.get('where')).toBeDefined();
+    expect(component.originFormGroup.get('origin')).toBeDefined();
+    expect(component.whenFormGroup.get('when')).toBeDefined();
+    expect(component.passengersFormGroup.get('slider')).toBeDefined();
+  }));
+
+  it('should submit when all forms are valid', () => {
+    component.ngOnInit();
+
+    const privateComponent: any = component;
+
+    privateComponent._flightSearchSubmitService = {
+      submitFlightSearchDataToApi: jest.fn().mockReturnValue(of({})),
+      serializeFlightSearchData: jest.fn(),
+    };
+
+    component.whereFormGroup.setControl('where', new FormControl('test'));
+    component.originFormGroup.setControl('origin', new FormControl('test'));
+    component.whenFormGroup.setControl(
+      'when',
+      new FormControl({ start: new Date(), end: new Date() }),
+    );
+    component.passengersFormGroup.setControl('slider', new FormControl(1));
+
+    component.submit();
+
+    expect(
+      privateComponent._flightSearchSubmitService.submitFlightSearchDataToApi,
+    ).toHaveBeenCalled();
+  });
+
+  it('should handle errors on submission failure', () => {
+    component.ngOnInit();
+
+    component['_flightSearchSubmitService'].serializeFlightSearchData = jest
+      .fn()
+      .mockImplementation(() => {
+        return {};
+      });
+
+    component['_flightSearchSubmitService'].submitFlightSearchDataToApi = jest
+      .fn()
+      .mockReturnValue(throwError(new Error('API error')));
+
+    component.whereFormGroup.setControl('where', new FormControl('test'));
+    component.originFormGroup.setControl('origin', new FormControl('test'));
+    component.whenFormGroup.setControl(
+      'when',
+      new FormControl({ start: new Date(), end: new Date() }),
+    );
+    component.passengersFormGroup.setControl('slider', new FormControl(1));
+
+    const consoleErrorSpy = jest.spyOn(console, 'error');
+
+    component.submit();
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      'Submit Error:',
+      new Error('API error'),
+    );
+  });
+
+  it('should open snackbar with error message when form is invalid', async () => {
+    component.ngAfterViewInit();
+
+    component.whereFormGroup.setControl('where', new FormControl(null));
+    component.originFormGroup.setControl('origin', new FormControl(null));
+    component.whenFormGroup.setControl(
+      'when',
+      new FormControl({ start: new Date(), end: new Date() }),
+    );
+    component.passengersFormGroup.setControl('slider', new FormControl(1));
+
+    component['_flightSearchSubmitService'].serializeFlightSearchData = jest
+      .fn()
+      .mockImplementation(() => ({}));
+    component['_flightSearchSubmitService'].submitFlightSearchDataToApi = jest
+      .fn()
+      .mockReturnValue(of({}));
+
+    component.submit();
+
+    const snackBarOpenSpy = jest.spyOn(component['_snackBar'], 'open');
+
+    expect(snackBarOpenSpy).toHaveBeenCalledWith(
+      'Please correct the errors in the form before submitting.',
+      'Close',
+      expect.any(Object),
+    );
+  });
+});
