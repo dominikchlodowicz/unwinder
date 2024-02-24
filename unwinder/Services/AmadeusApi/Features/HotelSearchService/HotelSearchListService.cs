478639f2d4e2000033014c1039f066d4aa04e206
dominikchlodowicz <dominikchlodowicz@gmail.com>
2024-02-24 13:36:39 +0100

    Split HotelSearchService into HotelSearchService and HotelSearchListService.

---
+++ unwinder/Services/AmadeusApi/Features/HotelSearchService/HotelSearchListService.cs
@@ -0,0 +1,12 @@
+using unwidner.Models.AmadeusApiServiceModels.HotelSearchModels;
+
+namespace unwinder.Services.AmadeusApiService.HotelSearch;
+
+public class HotelSearchListService : IHotelSearchListService
+{
+    public Task SearchListOfHotels
+            (HotelSearchListParametersModel hotelSearchListParametersModel)
+    {
+        throw new NotImplementedException();
+    }
+}
--- unwinder/Services/AmadeusApi/Features/HotelSearchService/HotelSearchService.cs
+++ unwinder/Services/AmadeusApi/Features/HotelSearchService/HotelSearchService.cs
@@ -2,17 +2,11 @@ using unwidner.Models.AmadeusApiServiceModels.HotelSearchModels;
 
 namespace unwinder.Services.AmadeusApiService.HotelSearch;
 
-public class HotelSearchService : IHotelSearchService, IHotelSearchListService
+public class HotelSearchService : IHotelSearchService
 {
     public HotelSearchService()
     {
 
-    }
-
-    public Task SearchListOfHotels
-            (HotelSearchListParametersModel hotelSearchListParametersModel)
-    {
-        throw new NotImplementedException();
     }
 
     public Task SearchHotel
